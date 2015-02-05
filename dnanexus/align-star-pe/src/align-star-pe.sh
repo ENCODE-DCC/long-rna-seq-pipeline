1f183a84c864926cf44f4757ee8ec06b9d041536
tdreszer <tdreszer@yahoo.com>
2015-02-05 03:59:08 +0000

    Interim check-in.  Covers experimental changes for a metadata output class string containing misc. json.

--- dnanexus/align-star-pe/src/align-star-pe.sh
+++ dnanexus/align-star-pe/src/align-star-pe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# align-star-pe 1.0.2
+# align-star-pe 1.1.0
 
 main() {
     # Now in resources/usr/bin
@@ -10,7 +10,7 @@ main() {
     #wget https://github.com/samtools/samtools/archive/0.1.19.tar.gz
 
     echo "*****"
-    echo "* Running: align-star-pe.sh [v1.0.2]"
+    echo "* Running: align-star-pe.sh [v1.1.0]"
     echo "* STAR version: "`STAR --version | awk '{print $1}' | cut -d _ -f 2-`
     echo "* samtools version: "`samtools 2>&1 | grep Version | awk '{print $2}'`
     echo "*****"
@@ -66,6 +66,102 @@ main() {
             awk '{printf $0 " "; getline; print}' | \
             sort -S 60G -T ./ | tr ' ' '\n' ) | \
         samtools view -@ ${nthreads} -bS - > ${reads1_fn}-${reads2_fn}_star_anno.bam
+    
+    # Gather metrics
+    meta=`echo \"STAR stats\": { `
+    #                      Number of input reads |       85357051
+    var=`grep "Number of input reads" Log.final.out | awk '{print $6}'`
+    var=`echo \"Number of input reads\": $var`
+    meta=`echo $meta $var`
+    #                  Average input read length |       202
+    var=`grep "Average input read length" Log.final.out | awk '{print $6}'`
+    var=`echo \"Average input read length\": $var`
+    meta=`echo $meta, $var`
+    #               Uniquely mapped reads number |       68649148
+    var=`grep "Uniquely mapped reads number" Log.final.out | awk '{print $6}'`
+    var=`echo \"Uniquely mapped reads number\": $var`
+    meta=`echo $meta, $var`
+    #                    Uniquely mapped reads % |       80.43%
+    var=`grep "Uniquely mapped reads \%" Log.final.out | awk '{print $6}'`
+    var=`echo \"Uniquely mapped reads percent\": \"$var\"`
+    meta=`echo $meta, $var`
+    #                      Average mapped length |       197.40
+    var=`grep "Average mapped length" Log.final.out | awk '{print $5}'`
+    var=`echo \"Average mapped length\": $var`
+    meta=`echo $meta, $var`
+    #                   Number of splices: Total |       30116609
+    var=`grep "Number of splices\: Total" Log.final.out | awk '{print $6}'`
+    var=`echo \"Number of splices total\": $var`
+    meta=`echo $meta, $var`
+    #        Number of splices: Annotated (sjdb) |       29770156
+    var=`grep "Number of splices\: Annotated" Log.final.out | awk '{print $7}'`
+    var=`echo \"Number of splices annotated\": $var`
+    meta=`echo $meta, $var`
+    #                   Number of splices: GT/AG |       29847443
+    var=`grep "Number of splices\: GT\/AG" Log.final.out | awk '{print $6}'`
+    var=`echo \"Number of splices GT/AG\": $var`
+    meta=`echo $meta, $var`
+    #                   Number of splices: GC/AG |       207291
+    var=`grep "Number of splices\: GC\/AG" Log.final.out | awk '{print $6}'`
+    var=`echo \"Number of splices GC/AG\": $var`
+    meta=`echo $meta, $var`
+    #                   Number of splices: AT/AC |       35287
+    var=`grep "Number of splices\: AT\/AC" Log.final.out | awk '{print $6}'`
+    var=`echo \"Number of splices AT/AC\": $var`
+    meta=`echo $meta, $var`
+    #           Number of splices: Non-canonical |       26588
+    var=`grep "Number of splices\: Non\-canonical" Log.final.out | awk '{print $6}'`
+    var=`echo \"Number of splices non-canonical\": $var`
+    meta=`echo $meta, $var`
+    #                  Mismatch rate per base, % |       0.62%
+    var=`grep "Mismatch rate per base" Log.final.out | awk '{print $7}'`
+    var=`echo \"Mismatch rate per base\": \"$var\"`
+    meta=`echo $meta, $var`
+    #                     Deletion rate per base |       0.01%
+    var=`grep "Deletion rate per base" Log.final.out | awk '{print $6}'`
+    var=`echo \"Deletion rate per base\": \"$var\"`
+    meta=`echo $meta, $var`
+    #                    Deletion average length |       1.89
+    var=`grep "Deletion average length" Log.final.out | awk '{print $5}'`
+    var=`echo \"Deletion average length\": $var`
+    meta=`echo $meta, $var`
+    #                    Insertion rate per base |       0.02%
+    var=`grep "Insertion rate per base" Log.final.out | awk '{print $6}'`
+    var=`echo \"Insertion rate per base\": \"$var\"`
+    meta=`echo $meta, $var`
+    #                   Insertion average length |       1.60
+    var=`grep "Insertion average length" Log.final.out | awk '{print $5}'`
+    var=`echo \"Insertion average length\": $var`
+    meta=`echo $meta, $var`
+    #    Number of reads mapped to multiple loci |       3936606
+    var=`grep "Number of reads mapped to multiple loci" Log.final.out | awk '{print $9}'`
+    var=`echo \"Number of reads mapped to multiple loci\": $var`
+    meta=`echo $meta, $var`
+    #         % of reads mapped to multiple loci |       4.61%
+    var=`grep "\% of reads mapped to multiple loci" Log.final.out | awk '{print $9}'`
+    var=`echo \"Percent of reads mapped to multiple loci\": \"$var\"`
+    meta=`echo $meta, $var`
+    #    Number of reads mapped to too many loci |       10508
+    var=`grep "Number of reads mapped to too many loci" Log.final.out | awk '{print $10}'`
+    var=`echo \"Number of reads mapped to too many loci\": $var`
+    meta=`echo $meta, $var`
+    #         % of reads mapped to too many loci |       0.01%
+    var=`grep "\% of reads mapped to too many loci" Log.final.out | awk '{print $10}'`
+    var=`echo \"Percent of reads mapped to too many loci\": \"$var\"`
+    meta=`echo $meta, $var`
+    #   % of reads unmapped: too many mismatches |       0.00%
+    var=`grep "\% of reads unmapped\: too many mismatches" Log.final.out | awk '{print $9}'`
+    var=`echo \"Percent of reads unmapped - too many mismatches\": \"$var\"`
+    meta=`echo $meta, $var`
+    #             % of reads unmapped: too short |       14.86%
+    var=`grep "\% of reads unmapped\: too short" Log.final.out | awk '{print $8}'`
+    var=`echo \"Percent reads unmapped - too short\": \"$var\"`
+    meta=`echo $meta, $var`
+    #                 % of reads unmapped: other |       0.09%
+    var=`grep "\% of reads unmapped\: other" Log.final.out | awk '{print $7}'`
+    var=`echo \"Percent of reads unmapped - other\": \"$var\"`
+    meta=`echo $meta, $var`
+    meta=`echo $meta }`
 
     #mv Aligned.toTranscriptome.out.bam ${reads1_fn}-${reads2_fn}_star_anno.bam
     mv Log.final.out ${reads1_fn}-${reads2_fn}_star_Log.final.out
@@ -83,5 +179,6 @@ main() {
     dx-jobutil-add-output star_genome_bam "$star_genome_bam" --class=file
     #dx-jobutil-add-output star_genome_bai "$star_genome_bai" --class=file
     dx-jobutil-add-output star_anno_bam "$star_anno_bam" --class=file
+    dx-jobutil-add-output metadata "$meta" --class=string
     echo "* Finished."
 }
