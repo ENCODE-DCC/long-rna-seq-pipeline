e5fab238cf86c5a081f958ab6bbfc6c93e652a92
tdreszer <tdreszer@yahoo.com>
2015-06-03 19:42:58 +0000

    The 3rd party sw call_peaks was reved to version 2.0.4 so the applet version had to jump from 1.0.1 to 1.1.0.

--- dnanexus/rampage/rampage-align-pe/src/rampage-align-pe.sh
+++ dnanexus/rampage/rampage-align-pe/src/rampage-align-pe.sh
@@ -2,7 +2,7 @@
 # rampage-align-pe.sh
 
 script_name="rampage-align-pe.sh"
-script_ver="1.0.0"
+script_ver="1.0.1"
 
 main() {
     # Now in resources/usr/bin
@@ -17,11 +17,6 @@ main() {
     if [ -f /usr/bin/tool_versions.py ]; then 
         versions=`tool_versions.py --applet $script_name --appver $script_ver`
     fi
-    #echo "*****"
-    #echo "* Running: rampage-align-pe.sh [v1.0.0]"
-    #echo "* STAR version: "`STAR --version | awk '{print $1}' | cut -d _ -f 2-`
-    ##echo "* samtools version: "`samtools 2>&1 | grep Version | awk '{print $2}'`
-    #echo "*****"
 
     echo "* Value of reads_1: '$reads_1'"
     echo "* Value of reads_2: '$reads_2'"
@@ -83,101 +78,6 @@ main() {
     if [ -f /usr/bin/qc_metrics.py ]; then
         meta=`qc_metrics.py -n STAR_log_final -f Log.final.out`
     fi
-    ## Gather metrics
-    #meta=`echo \"STAR_log_final\": { `
-    ##                      Number of input reads |       85357051
-    #var=`grep "Number of input reads" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Number of input reads\": $var`
-    #meta=`echo $meta $var`
-    ##                  Average input read length |       202
-    #var=`grep "Average input read length" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Average input read length\": $var`
-    #meta=`echo $meta, $var`
-    ##               Uniquely mapped reads number |       68649148
-    #var=`grep "Uniquely mapped reads number" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Uniquely mapped reads number\": $var`
-    #meta=`echo $meta, $var`
-    ##                    Uniquely mapped reads % |       80.43%
-    #var=`grep "Uniquely mapped reads \%" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Uniquely mapped reads percent\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##                      Average mapped length |       197.40
-    #var=`grep "Average mapped length" Log.final.out | awk '{print $5}'`
-    #var=`echo \"Average mapped length\": $var`
-    #meta=`echo $meta, $var`
-    ##                   Number of splices: Total |       30116609
-    #var=`grep "Number of splices\: Total" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Number of splices total\": $var`
-    #meta=`echo $meta, $var`
-    ##        Number of splices: Annotated (sjdb) |       29770156
-    #var=`grep "Number of splices\: Annotated" Log.final.out | awk '{print $7}'`
-    #var=`echo \"Number of splices annotated\": $var`
-    #meta=`echo $meta, $var`
-    ##                   Number of splices: GT/AG |       29847443
-    #var=`grep "Number of splices\: GT\/AG" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Number of splices GT/AG\": $var`
-    #meta=`echo $meta, $var`
-    ##                   Number of splices: GC/AG |       207291
-    #var=`grep "Number of splices\: GC\/AG" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Number of splices GC/AG\": $var`
-    #meta=`echo $meta, $var`
-    ##                   Number of splices: AT/AC |       35287
-    #var=`grep "Number of splices\: AT\/AC" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Number of splices AT/AC\": $var`
-    #meta=`echo $meta, $var`
-    ##           Number of splices: Non-canonical |       26588
-    #var=`grep "Number of splices\: Non\-canonical" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Number of splices non-canonical\": $var`
-    #meta=`echo $meta, $var`
-    ##                  Mismatch rate per base, % |       0.62%
-    #var=`grep "Mismatch rate per base" Log.final.out | awk '{print $7}'`
-    #var=`echo \"Mismatch rate per base\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##                     Deletion rate per base |       0.01%
-    #var=`grep "Deletion rate per base" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Deletion rate per base\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##                    Deletion average length |       1.89
-    #var=`grep "Deletion average length" Log.final.out | awk '{print $5}'`
-    #var=`echo \"Deletion average length\": $var`
-    #meta=`echo $meta, $var`
-    ##                    Insertion rate per base |       0.02%
-    #var=`grep "Insertion rate per base" Log.final.out | awk '{print $6}'`
-    #var=`echo \"Insertion rate per base\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##                   Insertion average length |       1.60
-    #var=`grep "Insertion average length" Log.final.out | awk '{print $5}'`
-    #var=`echo \"Insertion average length\": $var`
-    #meta=`echo $meta, $var`
-    ##    Number of reads mapped to multiple loci |       3936606
-    #var=`grep "Number of reads mapped to multiple loci" Log.final.out | awk '{print $9}'`
-    #var=`echo \"Number of reads mapped to multiple loci\": $var`
-    #meta=`echo $meta, $var`
-    ##         % of reads mapped to multiple loci |       4.61%
-    #var=`grep "\% of reads mapped to multiple loci" Log.final.out | awk '{print $9}'`
-    #var=`echo \"Percent of reads mapped to multiple loci\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##    Number of reads mapped to too many loci |       10508
-    #var=`grep "Number of reads mapped to too many loci" Log.final.out | awk '{print $10}'`
-    #var=`echo \"Number of reads mapped to too many loci\": $var`
-    #meta=`echo $meta, $var`
-    ##         % of reads mapped to too many loci |       0.01%
-    #var=`grep "\% of reads mapped to too many loci" Log.final.out | awk '{print $10}'`
-    #var=`echo \"Percent of reads mapped to too many loci\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##   % of reads unmapped: too many mismatches |       0.00%
-    #var=`grep "\% of reads unmapped\: too many mismatches" Log.final.out | awk '{print $9}'`
-    #var=`echo \"Percent of reads unmapped - too many mismatches\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##             % of reads unmapped: too short |       14.86%
-    #var=`grep "\% of reads unmapped\: too short" Log.final.out | awk '{print $8}'`
-    #var=`echo \"Percent reads unmapped - too short\": \"$var\"`
-    #meta=`echo $meta, $var`
-    ##                 % of reads unmapped: other |       0.09%
-    #var=`grep "\% of reads unmapped\: other" Log.final.out | awk '{print $7}'`
-    #var=`echo \"Percent of reads unmapped - other\": \"$var\"`
-    #meta=`echo $meta, $var`
-    #meta=`echo $meta }`
 
     #mv Aligned.toTranscriptome.out.bam ${reads_fn}_star_anno.bam
     set -x
