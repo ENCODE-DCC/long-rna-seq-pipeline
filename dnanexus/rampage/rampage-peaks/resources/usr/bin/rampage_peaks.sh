20390926ad9d80a2078efc11ac76ccfd4349dc2e
tdreszer <tdreszer@yahoo.com>
2016-08-15 17:44:02 +0000

    Because validateFiles fails on too long of bed lines, we now trim column 11 of tss_peaks bed to first 512 bytes

--- dnanexus/rampage/rampage-peaks/resources/usr/bin/rampage_peaks.sh
+++ dnanexus/rampage/rampage-peaks/resources/usr/bin/rampage_peaks.sh
@@ -55,9 +55,11 @@ call_peaks $reads_flag $rampage_bam --rnaseq-reads $control_bam --threads $ncpus
 set +x
  
 echo "-- Removing 'chrphiX' from bed..."
+# Because validateFiles fails on too long of bed lines, we now trim column 11 to first 512 bytes
 set -x
 grep -v "^track" ${peaks_root}.bed | sort -k1,1 -k2,2n > peaks.bed
-mv peaks.bed ${peaks_root}.bed # Necessary to avoid validation error
+cat peaks.bed | awk -F '\t' '{printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%.512s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11}' \
+                > ${peaks_root}.bed
 set +x
 
 echo "-- Converting bed to bigBed..."
